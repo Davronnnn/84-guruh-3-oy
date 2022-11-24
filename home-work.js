90aabcf2fc82d9da49d7d12d9b50291b76348d27
davronnnn <kifleader@gmail.com>
2022-11-24 19:32:13 +0500

    home-work

--- home-work.js
+++ home-work.js
@@ -1,61 +1,87 @@
+const elInput = document.querySelector('#number');
 const elForm = document.querySelector('form');
-// const elInput = document.querySelector('#number');
+const elBox1 = document.querySelector('.box1');
+const elBox2 = document.querySelector('.box2');
+const elBtns = document.querySelector('.btns');
 
-const elPerson = document.querySelector('.person');
-const elBicycle = document.querySelector('.bicycle');
-const elCar = document.querySelector('.car');
-const elPlain = document.querySelector('.plain');
-const elError = document.querySelector('.error');
+// for (let i = 0; i < 10; i++) {
+// 	const input = document.createElement('input');
+// 	input.setAttribute('type', 'button');
+// 	input.setAttribute('value', i);
+// 	input.setAttribute('id', i);
 
-const personSpeed = 3.6;
-const bicycleSpeed = 20.1;
-const carSpeed = 70;
-const plainSpeed = 800;
+// 	elBtns.appendChild(input);
+// }
+
+const elBtn0 = document.querySelector('#b0');
+const elBtn1 = document.querySelector('#b1');
+const elBtn2 = document.querySelector('#b2');
+const elBtn3 = document.querySelector('#b3');
+const elBtn4 = document.querySelector('#b4');
+const elBtn5 = document.querySelector('#b5');
+const elBtn6 = document.querySelector('#b6');
+const elBtn7 = document.querySelector('#b7');
+const elBtn8 = document.querySelector('#b8');
+const elBtn9 = document.querySelector('#b9');
+
+elBtn0.addEventListener('click', function () {
+	console.log(elInput.value);
+	if (elInput.value < 10) {
+		elInput.value += elBtn0.value;
+	}
+});
+
+elBtn1.addEventListener('click', function () {
+	console.log(elInput.value);
+	if (elInput.value < 10) {
+		elInput.value += elBtn1.value;
+	}
+});
+elBtn2.addEventListener('click', function () {
+	if (elInput.value < 10) {
+		elInput.value += elBtn2.value;
+	}
+});
+elBtn3.addEventListener('click', function () {
+	elInput.value += elBtn3.value;
+});
+elBtn4.addEventListener('click', function () {
+	elInput.value += elBtn4.value;
+});
+elBtn5.addEventListener('click', function () {
+	elInput.value += elBtn5.value;
+});
+elBtn6.addEventListener('click', function () {
+	elInput.value += elBtn6.value;
+});
+elBtn7.addEventListener('click', function () {
+	elInput.value += elBtn7.value;
+});
+elBtn8.addEventListener('click', function () {
+	elInput.value += elBtn8.value;
+});
+elBtn9.addEventListener('click', function () {
+	elInput.value += elBtn9.value;
+});
 
 elForm.addEventListener('submit', function (e) {
 	e.preventDefault();
 
-	const space = Number(e.target.input.value);
-
-	if (space == '' || isNaN(space)) {
-		elError.textContent = 'iltimos son kiriting';
+	if (
+		elInput.value !== '' &&
+		elInput.value >= 0 &&
+		elInput.value < 100 &&
+		!isNaN(Number(elInput.value))
+	) {
+		const value = Number(elInput.value);
+		if (value < 10) {
+			elBox1.style.border = '3px solid blue';
+			elBox2.style.border = '3px solid transparent';
+		} else if (value >= 10 && value < 100) {
+			elBox2.style.border = '3px solid blue';
+			elBox1.style.border = '3px solid transparent';
+		}
 	} else {
-		elError.textContent = '';
-		const personTime = space / personSpeed;
-		const personHour = Math.floor(personTime);
-		const personTotalMinute = personTime - personHour;
-		const personMinute = Math.floor(personTotalMinute * 60);
-		const personSecond = Math.floor(
-			(personTotalMinute * 60 - personMinute) * 60
-		);
-
-		const bicycleTime = space / bicycleSpeed;
-		const bicycleHour = Math.floor(bicycleTime);
-		const bicycleTotalMinute = bicycleTime - bicycleHour;
-		const bicycleMinute = Math.floor(bicycleTotalMinute * 60);
-		const bicycleSecond = Math.floor(
-			(bicycleTotalMinute * 60 - bicycleMinute) * 60
-		);
-
-		const carTime = space / carSpeed;
-		const carHour = Math.floor(carTime);
-		const carTotalMinute = carTime - carHour;
-		const carMinute = Math.floor(carTotalMinute * 60);
-		const carSecond = Math.floor((carTotalMinute * 60 - carMinute) * 60);
-
-		const plainTime = space / plainSpeed;
-		const plainHour = Math.floor(plainTime);
-		const plainTotalMinute = plainTime - plainHour;
-		const plainMinute = Math.floor(plainTotalMinute * 60);
-		const plainSecond = Math.floor(
-			(plainTotalMinute * 60 - plainMinute) * 60
-		);
-
-		// console.log();
-
-		elPerson.textContent = `${personHour} soat, ${personMinute} daqiqa, ${personSecond} soniya`;
-		elBicycle.textContent = `${bicycleHour} soat, ${bicycleMinute} daqiqa, ${bicycleSecond} soniya`;
-		elCar.textContent = `${carHour} soat, ${carMinute} daqiqa, ${carSecond} soniya`;
-		elPlain.textContent = `${plainHour} soat, ${plainMinute} daqiqa, ${plainSecond} soniya`;
+		alert('xato qiymat');
 	}
 });
